0c1c2d6af836fe0654501e024a2bbaa6710f89db
Chris Barber <christopherbarber4@googlemail.com>
2026-04-01 12:00:45 +0100

    basic landing page in place

--- 40486998/src/web/app.js
+++ 40486998/src/web/app.js
@@ -1,6 +1,21 @@
 import express from "express";
 const app = express(); // instatiate express
 import mysql from "mysql2"; // import mysql modulees
-import sessions from "express-session:";
+import sessions from "express-session";
+
+const PORT = 3000;
+
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
 
 
+app.get("/", (req,res) =>{
+
+    res.render("login");
+})
+
+
+app.listen(PORT, () =>{
+    console.log(`Server is live! http://localhost:${PORT}`);
+})
