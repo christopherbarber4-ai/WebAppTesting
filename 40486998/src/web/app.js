fd589243593c223a5c6f916d2e0056de86382aaf
Chris Barber <christopherbarber4@googlemail.com>
2026-04-01 12:29:02 +0100

    barebones login page with form that will submit

--- 40486998/src/web/app.js
+++ 40486998/src/web/app.js
@@ -10,12 +10,36 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
 
-app.get("/", (req,res) =>{
+app.get("/", (req, res) => {
 
     res.render("login");
 })
 
+app.post("/login", async (req, res) => {
+    const userEmail = req.body.userEmail;
+    const userPass = req.body.password;
 
-app.listen(PORT, () =>{
+    if (userPass === "123") {
+        res.send(`<h2> ${userEmail} tried to login </h2>
+        
+        <a href= "http://localhost:3000"> Back to login `);
+    } else {
+
+        res.redirect("/");
+    }
+
+
+});
+
+
+
+
+app.get("/landing", (req, res) => {
+
+    res.render("landing");
+})
+
+
+app.listen(PORT, () => {
     console.log(`Server is live! http://localhost:${PORT}`);
 })
