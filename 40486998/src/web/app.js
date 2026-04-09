9dc8437f54a986dafc044300493b5a22b686da15
Chris Barber <christopherbarber4@googlemail.com>
2026-04-09 16:57:22 +0100

    extended create classificsation route - now creates new award and links to  student

--- 40486998/src/web/app.js
+++ 40486998/src/web/app.js
@@ -13,8 +13,9 @@ app.use(express.static(path.join(__dirname, '/public')));
 const PORT = 3000;
 const oneHour = 10000 * 60 * 60 * 1; // variable for cookie timeout
 
-const RESITMAX = 40;
+const RESIT_MAX = 40;
 const YEARONEPASS = 4800;
+const TOTAL_CREDS = 120;
 
 const users = [{
     uName: "christopher.barber4@gmail.com",
@@ -82,15 +83,12 @@ app.get("/logout", (req, res) => {
 });
 
 app.get("/studentmgmt", async (req, res) => {
-    const studentssql = `SELECT * FROM student
+    const studentssql = `SELECT student.id AS stuID, firstName, lastName, email, courseID, graduationYear, awardID  FROM student
     INNER JOIN course 
     ON student.courseid = course.id
     INNER JOIN award
     ON student.awardid = award.id `;
     const [students] = await db.promise().query(studentssql);
-
-
-
     res.render("studentmgmt", { students });
 });
 
@@ -108,7 +106,7 @@ VALUES (?, ?, ?, ?, ?)`
     try {
         const [result] = await db.promise().query(insertStudentSQL, params)
         console.log(result);
-        res.send(`<H2> New user succesfully added </h2> <br> 
+        res.send(`<H2> New student succesfully added </h2> <br> 
                 click <a href = "/studentmgmt"> here </a> to return to student management `);
     } catch (error) {
         res.status(500).json(error);
@@ -117,19 +115,52 @@ VALUES (?, ?, ?, ?, ?)`
 
 });
 
-// get request when selecting edit from the Student Management page
+
 app.get("/editstudent/:eid", async (req, res) => {
     //const adminId <---- Need to add in authorisation;
     const studentId = req.params.eid;
+    const singlestudentSQL = `SELECT * FROM student WHERE id = ?`
+    const [student] = await db.promise().query(singlestudentSQL, [studentId]);
+    const coursesql = `SELECT * FROM course`
+    const [courses] = await db.promise().query(coursesql);
 
-    const singleStudentSQL = `SELECT * FROM student
-    INNER JOIN course 
-    ON student.courseid = course.id
-    INNER JOIN award
-    ON student.awardid = award.id WHERE student.id = ? `;
-    const [studentParams] = await db.promise().query(singleStudentSQL, [studentId]);
-    res.render("studentupdate", { studentParams });
+    res.render("studentupdate", { student, courses });
 });
+
+app.post("/editstudent", async (req, res) => {
+    const updatestudentForm = { ...req.body };
+    const updateSQL = `UPDATE SystemUser SET firstName = ?, lastName = ?, 
+    email = ?, graduationYear = ?
+    WHERE id = ?`;
+    const updateParams = [updatestudentForm.studentFirstName,
+    updatestudentForm.studentLastName,
+    updatestudentForm.studentEmail,
+    updatestudentForm.studentGradYr,
+    updatestudentForm.studentid]
+
+    try {
+        const [result] = await db.promise().query(updateSQL, updateParams);
+        console.log(result);
+        res.send(`<H2> Changes have been succesfully made. </h2> <br>User 
+            ${updatestudentForm.studentid} has been updated to reflect:
+            <ul>
+            <li> First Name: ${updatestudentForm.studentFirstName}</li>
+            <li> Last Name: ${updatestudentForm.studentLastName}</li>
+            <li> Email Address: ${updatestudentForm.studentEmail}</li>
+            <li> Graduation Year: ${updatestudentForm.studentGradYr}</li>
+            </ul> <br> 
+                Please click <a href = "/studentmgmt"> here </a> to return to user
+                management `)
+
+    } catch (error) {
+        res.status(500).json(error);
+        console.log(error);
+    }
+
+
+});
+
+
 
 app.get("/officermgmt", async (req, res) => {
     const userSQL = `SELECT * FROM systemuser`
@@ -137,6 +168,17 @@ app.get("/officermgmt", async (req, res) => {
     const coursesql = `SELECT * FROM course`
     const [courses] = await db.promise().query(coursesql);
     res.render("officermgmt", { courses, users });
+});
+
+app.get("/editofficer/:eid", async (req, res) => {
+    //const adminId <---- Need to add in authorisation;
+    const officerId = req.params.eid;
+    const singleofficerSQL = `SELECT * FROM systemuser WHERE id = ?`
+    const [officer] = await db.promise().query(singleofficerSQL, [officerId]);
+    const coursesql = `SELECT * FROM course`
+    const [courses] = await db.promise().query(coursesql);
+
+    res.render("officerupdate", { officer, courses });
 });
 
 
@@ -151,88 +193,13 @@ app.get("/coursemgmt", async (req, res) => {
 });
 
 
-app.post("/addstudent", async (req, res) => {
-    const addStudentForm = { ...req.body };
-    const insertstudentSQL = `INSERT INTO systemuser (firstName, lastName, email, role, password)
-VALUES (?, ?, ?, ?, ?)`
-    const params = [
-        addStudentForm.studentFirstName, addStudentForm.studentLastName,
-        addStudentForm.studentEmail,
-        addStudentForm.studentRole,
-        addStudentForm.studentPassword];
 
-    try {
-        const [result] = await db.promise().query(insertstudentSQL, params)
-        console.log(result);
-        res.send(`<H2> New user succesfully added </h2> <br> 
-                click <a href = "/studentmgmt"> here </a> to return to user management `);
-    } catch (error) {
-        res.status(500).json(error);
-        console.log(error);
-    }
-
-});
-
-app.get("/editstudent/:eid", async (req, res) => {
-    //const adminId <---- Need to add in authorisation;
-    const studentId = req.params.eid;
-    const singlestudentSQL = `SELECT * FROM systemuser WHERE id = ?`
-    const [student] = await db.promise().query(singlestudentSQL, [studentId]);
-    const coursesql = `SELECT * FROM course`
-    const [courses] = await db.promise().query(coursesql);
-
-    res.render("studentupdate", { student, courses });
-});
-
-app.post("/editstudent", async (req, res) => {
-    const updatestudentForm = { ...req.body };
-    const updateSQL = `UPDATE SystemUser SET firstName = ?, lastName = ?, 
-    email = ?, role = ?
-    WHERE id = ?`;
-    const updateParams = [updatestudentForm.studentFirstName,
-    updatestudentForm.studentLastName,
-    updatestudentForm.studentEmail,
-    updatestudentForm.studentRole,
-    updatestudentForm.studentid]
-
-    try {
-        const [result] = await db.promise().query(updateSQL, updateParams);
-        console.log(result);
-        res.send(`<H2> Changes have been succesfully made. </h2> <br>User 
-            ${updatestudentForm.studentid} has been updated to reflect:
-            <ul>
-            <li> First Name: ${updatestudentForm.studentFirstName}</li>
-            <li> Last Name: ${updatestudentForm.studentLastName}</li>
-            <li> Email Address: ${updatestudentForm.studentEmail}</li>
-            <li> Role: ${updatestudentForm.studentRole}</li>
-            </ul> <br> 
-                Please click <a href = "/studentmgmt"> here </a> to return to user
-                management `)
-
-    } catch (error) {
-        res.status(500).json(error);
-        console.log(error);
-    }
-
-
-});
 
 //logic for auto classification
 
 //VIEW ALL RESULTS
-app.get("/resultsmgmt", async (req, res) => {
-    const resultsSQL = `SELECT * FROM results
-    INNER JOIN student 
-    ON results.studentId = student.id
-    INNER JOIN modules 
-    ON results.moduleID = modules.id`
-    const [totalResults] = await db.promise().query(resultsSQL);
-    console.log(totalResults);
 
-    res.render("resultsmgmt", { totalResults });
-});
-
-app.get("/updateresults/:eid", async (req, res) => {
+app.get("/viewresults/:eid", async (req, res) => {
     const studentId = req.params.eid;
     const resultsSQL = `SELECT * FROM results
     INNER JOIN student 
@@ -249,9 +216,16 @@ app.get("/updateresults/:eid", async (req, res) => {
             WHERE student.id = ?`;
 
     const [courses] = await db.promise().query(coursesql, [studentId]);
-    console.log(courses[0])
-    console.log(totalResults)
-    res.render("studentresults", { totalResults, courses });
+
+    const awardSQL = `SELECT * FROM student 
+    INNER JOIN award
+    ON student.awardID = award.id
+    WHERE student.id = ?`
+
+    const [award] = await db.promise().query(awardSQL, [studentId])
+
+    res.render("viewresults", { totalResults, courses, award });
+    console.log(award);
 });
 
 app.post("/addresult", async (req, res) => {
@@ -265,7 +239,6 @@ app.post("/addresult", async (req, res) => {
         addResultForm.moduleScore,
         addResultForm.isResit
         ];
-
 
     try {
         const [result] = await db.promise().query(insertResultSQL, params)
@@ -283,32 +256,21 @@ app.post("/addresult", async (req, res) => {
 
 });
 
-app.post("/updateClassification", async (req, res) => {
 
-    const calcYearScore = function (scores) {
-        return number * number;
-    };
-
-
-    const studentResultsSql = `SELECT * FROM Results
-    INNER JOIN student
-    ON results.studentId = student.id 
-    INNER JOIN modules
-    ON results.moduleID = modules.id
-
-    WHERE student.id = ? `;
-    const params = 1;
+app.post("/addclassification/", async (req, res) => {
+    const classificationForm = { ...req.body };
+    const studentId = [classificationForm.studentId];
+    const resultsSQL = `SELECT * FROM results
+    INNER JOIN student 
+    ON results.studentId = student.id
+    INNER JOIN modules 
+    ON results.moduleID = modules.id WHERE student.id = ?`;
+    let studentClassification;
 
     try {
-        const [results] = await db.promise().query(studentResultsSql, params)
-        console.log(results)
-        console.log(results.length);
+        const [results] = await db.promise().query(resultsSQL, [studentId]);
 
-
-        let yearOneResults = [];
-        let yearTwoResults = [];
-        let yearThreeResults = [];
-
+        //STEP 1 - ensure where resits, the resis result is taken forwards.
         const cleanedResults = new Map();
         results.forEach((result) => {
             if (!cleanedResults.has(result.moduleID) || result.resit === 1) {
@@ -317,65 +279,101 @@ app.post("/updateClassification", async (req, res) => {
         });
         const singleResults = Array.from(cleanedResults.values());
 
+        //STEP 2 - calculate the final scores for each year and total
+        let yearOneResults = 0;
+        let yearTwoResults = 0;
+        let yearThreeResults = 0;
+        let yearOneFail = false;
+
         singleResults.forEach((result) => {
             if (result.resit === 1 && result.score > 40) {
-                result.score = RESITMAX;
+                result.score = RESIT_MAX;
             } else {
                 result.score = result.score;
             }
             switch (result.year) {
                 case "Y1":
                     if (result.score < 40) {
-                        yearOneResults.push("FAIL");
+                        yearOneFail = true;
                     } else {
-                        yearOneResults.push(result.score * result.creditValue);
+                        yearOneResults += (parseFloat(result.score) * result.creditValue);
                     }
                     break;
                 case "Y2":
-                    yearTwoResults.push(result.score * result.creditValue);
+                    yearTwoResults += (parseFloat(result.score) * result.creditValue);
                     break;
                 case "Y3":
-                    yearThreeResults.push(result.score * result.creditValue);
+                    yearThreeResults += (parseFloat(result.score) * result.creditValue);
                     break;
 
             }
         });
-        console.log(yearOneResults);
-        console.log(yearTwoResults);
-        console.log(yearThreeResults);
-        let yearOneFail = false;
 
-        if (yearOneResults.includes("FAIL")) {
-            yearOneFail = true;
+        //STEP 3 - Confirm classification based on final score.
+        function calcFinalClassification(y1isFail, y2, y3, TOTAL_CREDS) {
+            let finalScore = 0;
+            let classification = "";
+
+            if (!y1isFail) {
+                finalScore += (y2 / TOTAL_CREDS * 0.30) + (y3 / TOTAL_CREDS * 0.70);
+            } else {
+                classification = "Fail";
+                return classification;
+            }
+
+            if (finalScore <= 39.99) {
+                classification = "Fail";
+            }
+            else if (finalScore >= 40 && finalScore <= 49.99) {
+                classification = "Third Class Honours";
+            }
+            else if (finalScore >= 50 && finalScore <= 59.99) {
+                classification = "Lower Second Class (2:2)";
+            }
+            else if (finalScore >= 60 && finalScore <= 69.99) {
+                classification = "Upper Second Class (2:1)";
+            }
+            else {
+                classification = "First Class Honours (1st)";
+            }
+            console.log(classification);
+            console.log(finalScore);
+            return classification;
         }
 
-
-        if (!yearOneFail) {
-            let yearTwoSum = 0;
-            yearTwoResults.forEach((result) =>
-                yearTwoSum += result);
-            let yearTwoFinal = (yearTwoSum / 120) * 0.30;
-            console.log(yearTwoFinal);
-
-            let yearThreeSum = 0;
-            yearThreeResults.forEach((result) =>
-                yearThreeSum += result);
-            let yearThreeFinal = (yearThreeSum / 120) * 0.70;
-            console.log(yearThreeFinal);
-
-            let finalClassification = yearTwoFinal + yearThreeFinal;
-            console.log(finalClassification);
-            res.send(`<h2> Classification achieved: ${finalClassification}</h2>`);
-        }
-        else {
-            console.log("Student did not pass y1");
-        }
-
+        studentClassification = calcFinalClassification(yearOneFail, yearTwoResults, yearThreeResults, TOTAL_CREDS);
 
     } catch (error) {
         res.status(500).json(error);
         console.log(error);
     }
+
+    const insertClassificationSQL = `INSERT INTO award (classification, classificationStatus, systemUserID) VALUES(?, ?, ?)`
+    const insertParams = [
+        studentClassification,
+        "In Progress",
+        3,
+    ];
+
+    try {
+        const [classificationresult] = await db.promise().query(insertClassificationSQL, insertParams)
+
+        const updateStudentSQL = `UPDATE student SET awardID = ? WHERE id = ?`;
+
+        const updateStudentParams = [classificationresult.insertId,
+        classificationForm.studentId];
+
+        const [updatestudentaward] = await db.promise().query(updateStudentSQL, updateStudentParams);
+
+        res.send(`<H2> New classification succesfully added </h2> <br>
+            Student ${classificationForm.studentId} has been updated with a ${studentClassification}
+                click <a href = "/studentmgmt"> here </a> to return to student management `);
+
+    } catch (error) {
+        res.status(500).json(error);
+        console.log(error);
+    }
+
 
 
 
