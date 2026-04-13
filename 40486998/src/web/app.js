d6f61fb8973bcc97db93a99d2546c98eaea18c41
Chris Barber <christopherbarber4@googlemail.com>
2026-04-13 15:48:18 +0100

    finished authentication, state adn authorisation

--- 40486998/src/web/app.js
+++ 40486998/src/web/app.js
@@ -16,6 +16,7 @@ const oneHour = 10000 * 60 * 60 * 1; // variable for cookie timeout
 const RESIT_MAX = 40;
 const TOTAL_CREDS = 120;
 
+let userAccessLevel = "";
 
 //SESSIONS SET UP
 
@@ -26,14 +27,14 @@ app.use(sessions({ // creates a session object on the node server
     resave: false
 }));
 
-    function checkAuth (req, res, next){
-    if (req.session.authen){
-    
+function checkAuth(req, res, next) {
+    if (req.session.authen) {
+
         next();
     } else {
- res.send(`<h2> Error, Access Denied </h2> <br>
+        res.send(`<h2> Error, Access Denied </h2> <br>
                 <a href="/"> back </a>`)
-}
+    }
 }
 
 //DB SET UP
@@ -56,127 +57,136 @@ app.get("/", (req, res) => {
     res.render("login");
 })
 
-    app.post("/login", async (req, res) => {
-        const userEmail = req.body.userEmail;
-        const checkLogin = `SELECT * FROM systemuser WHERE email = ?`
+app.get("/login", (req, res) => {
+    res.render("login");
+})
 
-        try {
-            const [rows] = await db.promise().query(checkLogin, [userEmail])
-            if (rows.length > 0) {
-                req.session.authen = rows[0].id;
-                res.redirect("/landing");
-            } else {
-                res.send(`<h2> error, login credentials incorrect`)
-            }
-        } catch (error) {
-            res.status(500).send(error);
-        }
+app.post("/login", async (req, res) => {
+    const userEmail = req.body.userEmail;
+    const userPass = req.body.userPassword;
+    const checkLoginSQL = `SELECT * FROM systemuser WHERE email = ?`
 
-    });
-
-
-
-    app.get("/landing", async (req, res) => {
-        if (req.session.authen) {
-            const uID = req.session.authen;
-            const checkLogin = `SELECT * FROM systemuser WHERE id = ?`
-            const [rows] = await db.promise().query(checkLogin, [uID])
-            const userData = rows[0];
-            res.render("landing", { userData });
-            
+    try {
+        const [rows] = await db.promise().query(checkLoginSQL, [userEmail])
+        if (rows.length > 0 && rows[0].password === userPass) {
+            rows[0].password == userPass;
+            req.session.authen = rows[0].id;
+            userAccessLevel = rows[0].role;
+            res.redirect("/landing");
         } else {
-            res.send(`<h2> Error, Access Denied </h2> <br>
-                <a href="/" back </a>`)
+            res.send(`<h2> error, login credentials incorrect`)
         }
+    } catch (error) {
+        res.status(500).send(error);
+    }
 
-    })
+});
+
+
+
+app.get("/landing", async (req, res) => {
+    if (req.session.authen) {
+        const uID = req.session.authen;
+        const checkLogin = `SELECT * FROM systemuser WHERE id = ?`
+        const [rows] = await db.promise().query(checkLogin, [uID])
+        const userData = rows[0];
+        res.render("landing", { userData, userAccessLevel });
+
+    } else {
+        res.send(`<h2> Error, Access Denied </h2> <br>
+                <a href="/" back </a>`)
+    }
+
+})
 
 app.get("/dashboard", checkAuth, async (req, res) => {
-    
-    const courseDataSQL = `SELECT course.id as courseID,student.id AS stuID, course.title, award.classification, award.finalScore, student.awardID FROM course
+
+    if (userAccessLevel === "officer(view)" || userAccessLevel === "officer(edit)" || userAccessLevel === "admin") {
+        const courseDataSQL = `SELECT course.id as courseID,student.id AS stuID, course.title, award.classification, award.finalScore, student.awardID FROM course
     LEFT JOIN student 
     ON student.courseID = course.id
     LEFT JOIN award
     ON student.awardID = award.id `;
-    const [stats] = await db.promise().query(courseDataSQL);
+        const [stats] = await db.promise().query(courseDataSQL);
 
 
-    //create an array to ensure only unique courses and also house
-    // counts per course e.g. grades and no. of students 
-    const studentStats = [];
-    let globalStudentCount = 0;
-    stats.forEach((stat) => {
+        //create an array to ensure only unique courses and also house
+        // counts per course e.g. grades and no. of students 
+        const studentStats = [];
+        let globalStudentCount = 0;
+        stats.forEach((stat) => {
 
-        if (!studentStats[stat.courseID]) {
-            studentStats[stat.courseID] = {
-                id: stat.courseID,
-                title: stat.title,
-                totalStudents: 0,
-                gradeFirst: 0,
-                gradeTwoOne: 0,
-                gradeTwoTwo: 0,
-                gradeThird: 0,
-                gradeFail: 0,
-                allGradedScores: 0,
-                gradedStudents: 0
-            };
-        }
-
-        if (stat.stuID) {
-            studentStats[stat.courseID].totalStudents++;
-            if (stat.finalScore != null) {
-                studentStats[stat.courseID].allGradedScores += parseFloat(stat.finalScore);
-                studentStats[stat.courseID].gradedStudents++;
-            }
-            globalStudentCount++;
-            if (stat.classification === 'First Class Honours (1st)') {
-                studentStats[stat.courseID].gradeFirst++;
-            }
-            if (stat.classification === 'Upper Second Class (2:1)') {
-                studentStats[stat.courseID].gradeTwoOne++;
-            }
-            if (stat.classification === 'Lower Second Class (2:2)') {
-                studentStats[stat.courseID].gradeTwoTwo++;
-            }
-            if (stat.classification === 'Third Class Honours') {
-                studentStats[stat.courseID].gradeThird++;
-            }
-            if (stat.classification === 'Fail') {
-                studentStats[stat.courseID].gradeFail++;
+            if (!studentStats[stat.courseID]) {
+                studentStats[stat.courseID] = {
+                    id: stat.courseID,
+                    title: stat.title,
+                    totalStudents: 0,
+                    gradeFirst: 0,
+                    gradeTwoOne: 0,
+                    gradeTwoTwo: 0,
+                    gradeThird: 0,
+                    gradeFail: 0,
+                    allGradedScores: 0,
+                    gradedStudents: 0
+                };
             }
 
-        }
-    });
-    //course also same as stat but labelled differently here for clarity. two loops to count 
-    // both number of students and also number of classifications for each course
-    studentStats.forEach((course) => {
-        course.studentCountPercentage = ((course.totalStudents / globalStudentCount) * 100).toFixed(0);
-        course.averageScore = (course.allGradedScores / course.gradedStudents).toFixed(2);
+            if (stat.stuID) {
+                studentStats[stat.courseID].totalStudents++;
+                if (stat.finalScore != null) {
+                    studentStats[stat.courseID].allGradedScores += parseFloat(stat.finalScore);
+                    studentStats[stat.courseID].gradedStudents++;
+                }
+                globalStudentCount++;
+                if (stat.classification === 'First Class Honours (1st)') {
+                    studentStats[stat.courseID].gradeFirst++;
+                }
+                if (stat.classification === 'Upper Second Class (2:1)') {
+                    studentStats[stat.courseID].gradeTwoOne++;
+                }
+                if (stat.classification === 'Lower Second Class (2:2)') {
+                    studentStats[stat.courseID].gradeTwoTwo++;
+                }
+                if (stat.classification === 'Third Class Honours') {
+                    studentStats[stat.courseID].gradeThird++;
+                }
+                if (stat.classification === 'Fail') {
+                    studentStats[stat.courseID].gradeFail++;
+                }
 
-    });
+            }
+        });
+        //course also same as stat but labelled differently here for clarity. two loops to count 
+        // both number of students and also number of classifications for each course
+        studentStats.forEach((course) => {
+            course.studentCountPercentage = ((course.totalStudents / globalStudentCount) * 100).toFixed(0);
+            course.averageScore = (course.allGradedScores / course.gradedStudents).toFixed(2);
 
-    studentStats.forEach((course) => {
-        if (course.totalStudents > 0) {
-            course.firstPercentage = (((course.gradeFirst / course.totalStudents) * 100).toFixed(0));
-            course.twoOnePercentage = (((course.gradeTwoOne / course.totalStudents) * 100).toFixed(0));
-            course.twoTwoPercentage = (((course.gradeTwoTwo / course.totalStudents) * 100).toFixed(0));
-            course.thirdPercentage = (((course.gradeThird / course.totalStudents) * 100).toFixed(0));
-            course.failPercentage = (((course.gradeFail / course.totalStudents) * 100).toFixed(0));
-        }
-        else {
-            course.firstPercentage = 0;
-            course.twoOnePercentage = 0;
-            course.twoTwoPercentage = 0;
-            course.thirdPercentage = 0;
-            course.failPercentage = 0;
-        }
+        });
 
-    });
-    console.log(stats);
-    console.log(studentStats);
+        studentStats.forEach((course) => {
+            if (course.totalStudents > 0) {
+                course.firstPercentage = (((course.gradeFirst / course.totalStudents) * 100).toFixed(0));
+                course.twoOnePercentage = (((course.gradeTwoOne / course.totalStudents) * 100).toFixed(0));
+                course.twoTwoPercentage = (((course.gradeTwoTwo / course.totalStudents) * 100).toFixed(0));
+                course.thirdPercentage = (((course.gradeThird / course.totalStudents) * 100).toFixed(0));
+                course.failPercentage = (((course.gradeFail / course.totalStudents) * 100).toFixed(0));
+            }
+            else {
+                course.firstPercentage = 0;
+                course.twoOnePercentage = 0;
+                course.twoTwoPercentage = 0;
+                course.thirdPercentage = 0;
+                course.failPercentage = 0;
+            }
 
+        });
 
-    res.render("dashboard", { studentStats });
+        res.render("dashboard", { studentStats, userAccessLevel });
+    } else {
+        res.send(`<h2> Error, Access Denied </h2> <br>
+                <a href="/" back </a>`)
+    }
 });
 
 app.get("/logout", (req, res) => {
@@ -186,14 +196,22 @@ app.get("/logout", (req, res) => {
 
 });
 
-app.get("/studentmgmt",checkAuth, async (req, res) => {
-    const studentssql = `SELECT student.id AS stuID, firstName, lastName, email, courseID, graduationYear, awardID, course.title, award.classification  FROM student
+app.get("/studentmgmt", checkAuth, async (req, res) => {
+    if (userAccessLevel === "officer(view)" || userAccessLevel === "officer(edit)") {
+
+        const studentssql = `SELECT student.id AS stuID, firstName, lastName, email, courseID, graduationYear, awardID, course.title, award.classification  FROM student
     INNER JOIN course 
     ON student.courseID = course.id
     LEFT JOIN award
     ON student.awardid = award.id `;
-    const [students] = await db.promise().query(studentssql);
-    res.render("studentmgmt", { students });
+        const [students] = await db.promise().query(studentssql);
+        res.render("studentmgmt", { students, userAccessLevel });
+
+    } else {
+        res.send(`<h2> Error, Access Denied </h2> <br>
+                <a href="/" back </a>`)
+
+    }
 });
 
 app.post("/addstudent", checkAuth, async (req, res) => {
@@ -221,14 +239,17 @@ VALUES (?, ?, ?, ?, ?)`
 
 
 app.get("/editstudent/:eid", checkAuth, async (req, res) => {
-    //const adminId <---- Need to add in authorisation;
-    const studentId = req.params.eid;
-    const singlestudentSQL = `SELECT * FROM student WHERE id = ?`
-    const [student] = await db.promise().query(singlestudentSQL, [studentId]);
-    const coursesql = `SELECT * FROM course`
-    const [courses] = await db.promise().query(coursesql);
-
-    res.render("studentupdate", { student, courses });
+    if (userAccessLevel === "officer(edit)") {
+        const studentId = req.params.eid;
+        const singlestudentSQL = `SELECT * FROM student WHERE id = ?`
+        const [student] = await db.promise().query(singlestudentSQL, [studentId]);
+        const coursesql = `SELECT * FROM course`
+        const [courses] = await db.promise().query(coursesql);
+        res.render("studentupdate", { student, courses, userAccessLevel });
+    } else {
+        res.send(`<h2> Error, Access Denied </h2> <br>
+                <a href="/" back </a>`)
+    }
 });
 
 app.post("/editstudent", checkAuth, async (req, res) => {
@@ -265,14 +286,19 @@ app.post("/editstudent", checkAuth, async (req, res) => {
 });
 
 app.get("/deletestudent/:eid", checkAuth, async (req, res) => {
-    //const adminId <---- Need to add in authorisation;
-    const studentId = req.params.eid;
-    const singlestudentSQL = `SELECT * FROM student WHERE id = ?`
-    const [student] = await db.promise().query(singlestudentSQL, [studentId]);
-    const coursesql = `SELECT * FROM course`
-    const [courses] = await db.promise().query(coursesql);
-    res.render("studentdelete", { student, courses });
-})
+    if (userAccessLevel === "officer(edit)") {
+        const studentId = req.params.eid;
+        const singlestudentSQL = `SELECT * FROM student WHERE id = ?`
+        const [student] = await db.promise().query(singlestudentSQL, [studentId]);
+        const coursesql = `SELECT * FROM course`
+        const [courses] = await db.promise().query(coursesql);
+        res.render("studentdelete", { student, courses, userAccessLevel });
+    }
+    else {
+        res.send(`<h2> Error, Access Denied </h2> <br>
+                <a href="/" back </a>`)
+    }
+});
 
 app.post("/deletestudent/", checkAuth, async (req, res) => {
     //const adminId <---- Need to add in authorisation;
@@ -294,11 +320,19 @@ app.post("/deletestudent/", checkAuth, async (req, res) => {
 
 
 app.get("/officermgmt", checkAuth, async (req, res) => {
-    const userSQL = `SELECT * FROM systemuser`
-    const [users] = await db.promise().query(userSQL);
-    const coursesql = `SELECT * FROM course`
-    const [courses] = await db.promise().query(coursesql);
-    res.render("officermgmt", { courses, users });
+    if (userAccessLevel === "admin") {
+
+
+        const userSQL = `SELECT * FROM systemuser`
+        const [users] = await db.promise().query(userSQL);
+        const coursesql = `SELECT * FROM course`
+        const [courses] = await db.promise().query(coursesql);
+        res.render("officermgmt", { courses, users, userAccessLevel });
+    }
+    else {
+        res.send(`<h2> Error, Access Denied </h2> <br>
+                <a href="/" back </a>`)
+    }
 });
 
 app.post("/addofficer", async (req, res) => {
@@ -338,14 +372,23 @@ VALUES (?, ?, ?, ?, ?)`
 
 
 app.get("/editofficer/:eid", checkAuth, async (req, res) => {
-    //const adminId <---- Need to add in authorisation;
-    const officerId = req.params.eid;
-    const singleofficerSQL = `SELECT * FROM systemuser WHERE id = ?`
-    const [officer] = await db.promise().query(singleofficerSQL, [officerId]);
-    const coursesql = `SELECT * FROM course`
-    const [courses] = await db.promise().query(coursesql);
 
-    res.render("officerupdate", { officer, courses });
+    if (userAccessLevel === "admin") {
+
+
+        const officerId = req.params.eid;
+        const singleofficerSQL = `SELECT * FROM systemuser WHERE id = ?`
+        const [officer] = await db.promise().query(singleofficerSQL, [officerId]);
+        const coursesql = `SELECT * FROM course`
+        const [courses] = await db.promise().query(coursesql);
+
+        res.render("officerupdate", { officer, courses, userAccessLevel });
+    }
+    else {
+        res.send(`<h2> Error, Access Denied </h2> <br>
+                <a href="/" back </a>`)
+
+    }
 });
 
 
@@ -400,12 +443,17 @@ app.post("/editofficer", checkAuth, async (req, res) => {
 });
 
 app.get("/deleteofficer/:eid", checkAuth, async (req, res) => {
-    //const adminId <---- Need to add in authorisation;
-    const officerId = req.params.eid;
-    const singleofficerSQL = `SELECT * FROM systemuser WHERE id = ?`
-    const [officer] = await db.promise().query(singleofficerSQL, [officerId]);
-    console.log(officer);
-    res.render("officerdelete", { officer });
+    if (userAccessLevel === "admin") {
+        const officerId = req.params.eid;
+        const singleofficerSQL = `SELECT * FROM systemuser WHERE id = ?`
+        const [officer] = await db.promise().query(singleofficerSQL, [officerId]);
+        console.log(officer);
+        res.render("officerdelete", { officer, userAccessLevel });
+    }
+    else {
+        res.send(`<h2> Error, Access Denied </h2> <br>
+                <a href="/" back </a>`)
+    }
 })
 
 app.post("/deleteofficer/", checkAuth, async (req, res) => {
@@ -436,49 +484,60 @@ app.post("/deleteofficer/", checkAuth, async (req, res) => {
 
 
 app.get("/coursemgmt", checkAuth, async (req, res) => {
-    const coursesql = `SELECT * FROM course
+    if (userAccessLevel === "officer(view)" || userAccessLevel === "officer(edit)") {
+        const coursesql = `SELECT * FROM course
     INNER JOIN Modules
     ON course.id = Modules.courseid WHERE course.title = ?`;
-    const params = "BSc Computer Science";
-    const [courses] = await db.promise().query(coursesql, [params]);
-    console.log(courses[0]);
-    res.render("coursemgmt", { courses });
+        const params = "BSc Computer Science";
+        const [courses] = await db.promise().query(coursesql, [params]);
+        console.log(courses[0]);
+        res.render("coursemgmt", { courses, userAccessLevel });
+    }
+    else {
+        res.send(`<h2> Error, Access Denied </h2> <br>
+                <a href="/" back </a>`)
+    }
 });
-
-
-
 
 //logic for auto classification
 
 //VIEW ALL RESULTS
 
 app.get("/viewresults/:eid", checkAuth, async (req, res) => {
-    const studentId = req.params.eid;
-    const resultsSQL = `SELECT * FROM results
+    if (userAccessLevel === "officer(view)" || userAccessLevel === "officer(edit)") {
+
+        const studentId = req.params.eid;
+        const resultsSQL = `SELECT * FROM results
     INNER JOIN student 
     ON results.studentId = student.id
     INNER JOIN modules 
     ON results.moduleID = modules.id WHERE student.id = ?`;
-    const [totalResults] = await db.promise().query(resultsSQL, [studentId]);
+        const [totalResults] = await db.promise().query(resultsSQL, [studentId]);
 
 
-    const coursesql = `SELECT modules.id AS moduleID, modules.moduleName, modules.creditValue, modules.year 
+        const coursesql = `SELECT modules.id AS moduleID, modules.moduleName, modules.creditValue, modules.year 
             FROM student
             INNER JOIN course ON student.courseID = course.id
             INNER JOIN modules ON modules.courseID = course.id
             WHERE student.id = ?`;
 
-    const [courses] = await db.promise().query(coursesql, [studentId]);
+        const [courses] = await db.promise().query(coursesql, [studentId]);
 
-    const awardSQL = `SELECT * FROM student 
+        const awardSQL = `SELECT * FROM student 
     INNER JOIN award
     ON student.awardID = award.id
     WHERE student.id = ?`
 
-    const [award] = await db.promise().query(awardSQL, [studentId])
+        const [award] = await db.promise().query(awardSQL, [studentId])
 
-    res.render("viewresults", { totalResults, courses, award });
-    console.log(award);
+        res.render("viewresults", { totalResults, courses, award, userAccessLevel });
+        console.log(award);
+    }
+
+    else {
+        res.send(`<h2> Error, Access Denied </h2> <br>
+                <a href="/" back </a>`)
+    }
 });
 
 app.post("/addresult", checkAuth, async (req, res) => {
